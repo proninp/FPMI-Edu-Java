304a0383abe178a8aaf44e865f1e404f11053dfb
proninp <proninpaul@yandex.ru>
2024-06-03 22:36:28 +0300

    Home Work 03 Java code 03 03.06.2024

--- Java Course/Home Works/HomeWork03/src/main/java/section4/Loops.java
+++ Java Course/Home Works/HomeWork03/src/main/java/section4/Loops.java
@@ -11,6 +11,19 @@ public class Loops {
         for (int i = 0; i <= x ; i++) {
             sb.append(i).append(" ");
         }
-        return sb.substring(0, sb.length() - 1);
+        return !sb.isEmpty() ? sb.substring(0, sb.length() - 1) : sb.toString();
+    }
+
+    /**
+     * 4.2 Числа наоборот
+     * @param x целое число
+     * @return возвращает строку, в которой будут записаны все числа от x до 0 (включительно)
+     */
+    public static String reverseListNums(int x) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = x; i >= 0 ; i--) {
+            sb.append(i).append(" ");
+        }
+        return !sb.isEmpty() ? sb.substring(0, sb.length() - 1) : sb.toString();
     }
 }
--- Java Course/Home Works/HomeWork03/src/test/java/section4/LoopsTest.java
+++ Java Course/Home Works/HomeWork03/src/test/java/section4/LoopsTest.java
@@ -10,5 +10,14 @@ class LoopsTest {
     @Test
     void listNums() {
         Assertions.assertEquals("0 1 2 3 4 5", Loops.listNums(5));
+        Assertions.assertEquals("0", Loops.listNums(0));
+        Assertions.assertEquals("", Loops.listNums(-1));
+    }
+
+    @Test
+    void reverseListNums() {
+        Assertions.assertEquals("5 4 3 2 1 0", Loops.reverseListNums(5));
+        Assertions.assertEquals("0", Loops.reverseListNums(0));
+        Assertions.assertEquals("", Loops.reverseListNums(-1));
     }
 }
